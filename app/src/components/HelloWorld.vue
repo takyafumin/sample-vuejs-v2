995e6380366eac9a6df2ff1ff1e059a8e9916fae
takyafumin <takyafumin@gmail.com>
2021-01-12 06:16:07 +0000

    initial commit

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () => {
+    return {
+      message: 'hello'
+    }
+  }
+}
+</script>

3e48179c2e012c20a0fbb7561f2826069e07304a
takyafumin <takyafumin@gmail.com>
2021-01-13 05:47:47 +0000

    add Exception, finally part

--- app/src/components/BitCoinApp.vue
+++ app/src/components/BitCoinApp.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h2>Bitcoin Price</h2>
-    <ul v-cloak>
-      <li v-for="(rate, currency) in bpi" v-bind:key="currency">
-        {{ currency }}: {{ rate.rate_float | currencyDecimal }}
-      </li>
-    </ul>
+    <section v-if="hasError">
+      Error.
+    </section>
+    <section v-else>
+      <div v-if="loading">
+        Loading...
+      </div>
+      <div v-else>
+        <ul v-cloak>
+          <li v-for="(rate, currency) in bpi" v-bind:key="currency">
+            {{ currency }}: {{ rate.rate_float | currencyDecimal }}
+          </li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -16,13 +26,22 @@ export default {
   name: 'BitCoinApp',
   data: () => {
     return {
-      bpi: null
+      bpi: null,
+      hasError: false,
+      loading: true
     }
   },
   mounted: function () {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(function (response) {
         this.bpi = response.data.bpi
+      }.bind(this))
+      .catch(function (error) {
+        console.log(error)
+        this.hasError = true
+      }.bind(this))
+      .finally(function () {
+        this.loading = false
       }.bind(this))
   },
   filters: {

7c196e2cfe7e7ffb1f4a2ae19dae97a5e83db4cc
takyafumin <takyafumin@gmail.com>
2021-01-13 01:22:12 +0000

    add sample - array

---
+++ app/src/components/SampleArray.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul v-for="(value, key) in colors" v-bind:key="key">
+      <li>{{ value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SampleArray',
+  data: () => {
+    return {
+      colors: [
+        'red',
+        'green',
+        'blue'
+      ]
+    }
+  }
+}
+</script>

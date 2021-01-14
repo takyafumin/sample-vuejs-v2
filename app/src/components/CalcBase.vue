e94cc8060b10b4753f785b0a3a36e2041cced5ca
takyafumin <takyafumin@gmail.com>
2021-01-14 01:22:10 +0000

    sample, Calc Property

---
+++ app/src/components/CalcBase.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <p>
+      {{ message.split('').reverse().join('') }}
+    </p>
+
+    <p>
+      {{ reversedMessage }}
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalcBase',
+  data: () => {
+    return {
+      message: 'Hello Vue.js!!'
+    }
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
